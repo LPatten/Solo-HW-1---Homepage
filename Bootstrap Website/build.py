b218f8d13c0d739bb527e9e633ce714b4cbb947a
Liam <you@example.com>
2018-06-03 15:26:20 -0700

    first draft of build.py document

---
+++ Bootstrap Website/build.py
@@ -0,0 +1,18 @@
+# First, get the template files
+top = open('templates/top.html').read()
+bottom = open('templates/bottom.html').read()
+
+#index file
+content = open('content/index.html').read()
+index_html = top + content + bottom
+open('index.html', 'w+').write(index_html)
+
+#experience file
+content = open('content/experience.html').read()
+experience_html = top + content + bottom
+open('blog.html', 'w+').write(experience_html)
+
+#projects file
+content = open('content/projects.html').read()
+projects_html = top + content + bottom
+open('art.html', 'w+').write(projects_html)
